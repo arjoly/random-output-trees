88c6b6973fb97846a47430a2ddf7810727a52463
Arnaud Joly <arnaud.v.joly@gmail.com>
2015-12-02 18:01:22 +0100

    simplify travis

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -16,40 +16,25 @@ export CC=gcc
 export CXX=g++
 
 sudo apt-get update -qq
-if [[ "$INSTALL_ATLAS" == "true" ]]; then
-    sudo apt-get install -qq libatlas3gf-base libatlas-dev
-fi
 
-if [[ "$DISTRIB" == "conda" ]]; then
-    # Deactivate the travis-provided virtual environment and setup a
-    # conda-based environment instead
-    deactivate
+# Deactivate the travis-provided virtual environment and setup a
+# conda-based environment instead
+deactivate
 
-    # Use the miniconda installer for faster download / install of conda
-    # itself
-    wget http://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh \
-        -O miniconda.sh
-    chmod +x miniconda.sh && ./miniconda.sh -b
-    export PATH=/home/travis/miniconda/bin:$PATH
-    conda update --yes conda
+# Use the miniconda installer for faster download / install of conda
+# itself
+wget http://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh \
+    -O miniconda.sh
+chmod +x miniconda.sh && ./miniconda.sh -b
+export PATH=/home/travis/miniconda/bin:$PATH
+conda update --yes conda
 
-    # Configure the conda environment and put it in the path using the
-    # provided versions
-    conda create -n testenv --yes python=$PYTHON_VERSION pip nose \
-        numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION
-    source activate testenv
+# Configure the conda environment and put it in the path using the
+# provided versions
+conda create -n testenv --yes python=$PYTHON_VERSION pip nose \
+    numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION scikit-learn
+source activate testenv
 
-
-elif [[ "$DISTRIB" == "ubuntu" ]]; then
-    # Use standard ubuntu packages in their default version
-    sudo apt-get install -qq python-scipy python-nose python-pip
-fi
-
-if [[ "$COVERAGE" == "true" ]]; then
-    pip install coverage coveralls
-fi
-
-pip install scikit-learn
 
 
 python --version
